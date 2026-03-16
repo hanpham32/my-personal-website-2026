ee64f9efccea87fd19a3c4d696be515c5901a384
Han (Omarchy) <hanpham3230@gmail.com>
2026-03-15 17:55:19 -0700

    revise homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,23 +5,15 @@ export function HomePage() {
       <p>
         Welcome to my little corner on the internet. I'm a software engineer
         based in Seattle. I'm skilled at full-stack development, building web
-        apps with web technologies (HTML, CSS, TypeScript, React, three.js) and
+        apps with web technologies (HTML, CSS, TypeScript, React, Three.js) and
         backend services in a variety of system languages. I lead product
         development and adoption at MoodRoom, a mental healthcare startup based
-        in Seattle. I recognize that my knowledge and understanding are limited
-        by what i don't know, so i strive to stay curious, ask questions, and
-        learn from others. Most importantly, i try to lead with kindness and
-        share with others along the way.
+        in Seattle.
       </p>
       <p>
-        This is where i share life stories, document my projects, and reflect on
-        what im learning. Topics that catch my attention include: blockchain,
-        economics, technology, etc.
-      </p>
-      <p>
-        I migrated from medium to bear blog because i love the creator's
-        philosophy of building software and services with care. I will do my
-        best to write and publish more here :^)
+        This space is also my canvas, my garden, my school. This is where I
+        share life stories, document projects/medias, and reflect on things that
+        I occupy my mind.
       </p>
       <p>Cheers</p>
       <p>Han</p>
